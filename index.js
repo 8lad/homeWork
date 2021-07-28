ac34374d2ada1cb86a911d1f2e8f2868e3114e6b
Vlad <vladisla1988@gmail.com>
2021-07-28 22:28:46 +0300

    added 3 tasks

---
+++ index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+// First task
+
+for (let i = 1; i <= 10; i++) {
+    const firstWord = 'Buz';
+    const secondWord = 'Fiz';
+    if ((i % 2 === 0 && i % 3 === 0) || i % 3 === 0) {
+        console.log(secondWord + firstWord);
+    } else if (i % 2 === 0) {
+        console.log(secondWord);
+    } else {
+        console.log(firstWord);
+    }
+}
+
+//  Second task
+
+const testNumber = 7;
+
+function findingFactorial(number) {
+    if (!number || number === null || number <= 0) {
+        return console.log('The variable is not correct');
+    }
+    for (let i = number - 1; i > 1; i--) {
+        number *= i;
+    }
+    console.log(number);
+}
+
+findingFactorial(testNumber);
+
+// Third task
+
+const sheetsInReamPaper = 500;
+const consumptionPerWeek = 1200;
+const weeksAmount = 8;
+
+
+function countingPaperPacks(paperInBox, weeklyUsage, weeks) {
+    let totalPaperSheets;
+    let paperBoxesAmount;
+
+    totalPaperSheets = weeklyUsage * weeks;
+    for (let i = 1; i > 0; i++) {
+        totalPaperSheets -= paperInBox;
+        if (totalPaperSheets <= 0) {
+            paperBoxesAmount = i;
+            if (paperBoxesAmount === 1) {
+                return console.log(`You need ${paperBoxesAmount} box with paper`);
+            }
+            if (paperBoxesAmount > 1) {
+                return console.log(`You need ${paperBoxesAmount} boxes with paper`);
+            }
+        }
+    }
+}
+
+countingPaperPacks(sheetsInReamPaper, consumptionPerWeek, weeksAmount);
+
+// Fourth task
+
+
+const roomsOnFloor = 3;
+const floors = 9;
+const roomNumber = 456;
+let porch;
+let floor;
+
+function findingAdress(roomsOnFloor, floors, roomNumber) {
+    let roomsInPorch = roomsOnFloor * floors;
+    let roomsFor = roomNumber;
+    let porchNumber;
+    let floorNumberLeft;
+    console.log(roomsInPorch, roomsFor);
+    for (let i = 1; i > 0; i++) {
+        roomsFor -= roomsInPorch;
+        if (roomsFor <= 0) {
+            floorNumberLeft = roomNumber % roomsInPorch;
+            porchNumber = i;
+            return { porchNumber, floorNumberLeft };
+        }
+    }
+    console.log(porchNumber, floorNumberLeft);
+}
+findingAdress(roomsOnFloor, floors, roomNumber);
