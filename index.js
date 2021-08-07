dac42dece100991ca6216eca44ecccb3915b73f5
Vlad <vladisla1988@gmail.com>
2021-08-07 22:59:03 +0300

    fixed the last task

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
 // The first task
 
 function Emploee(employee) {
-    this.id = employee.id;
-    this.name = employee.name;
-    this.surname = employee.surname;
-    this.salary = employee.salary;
-    this.workExperience = employee.workExperience;
-    this.isPrivileges = employee.isPrivileges;
-    this.gender = employee.gender;
+  this.id = employee.id;
+  this.name = employee.name;
+  this.surname = employee.surname;
+  this.salary = employee.salary;
+  this.workExperience = employee.workExperience;
+  this.isPrivileges = employee.isPrivileges;
+  this.gender = employee.gender;
 }
 
 // The second task
 
-Emploee.prototype.getFullName = function() {
-    return String(`${this.surname} ${this.name}`);
+Emploee.prototype.getFullName = function () {
+  return String(`${this.surname} ${this.name}`);
 };
 
 // The third task
 
 let createEmployesFromArr = (arr) => {
-    let result = arr.map(item => new Emploee(item));
-    return result;
+  let result = arr.map((item) => new Emploee(item));
+  return result;
 };
 const emplyeeConstructArr = createEmployesFromArr(emplyeeArr);
 
@@ -31,8 +31,8 @@ console.log(emplyeeConstructArr);
 // The fourth task
 
 const getFullNamesFromArr = (arr) => {
-    let result = arr.map(item => item.getFullName());
-    return result;
+  let result = arr.map((item) => item.getFullName());
+  return result;
 };
 
 getFullNamesFromArr(emplyeeConstructArr);
@@ -42,10 +42,10 @@ console.log(getFullNamesFromArr(emplyeeConstructArr));
 // The fifth task
 
 const getMiddleSalary = (arr) => {
-    let result = arr.map(item => item.salary);
-    let allSalary = result.reduce((sum, current) => sum + current);
-    let allMidleSalary = Math.trunc(allSalary / arr.length);
-    return allMidleSalary;
+  let result = arr.map((item) => item.salary);
+  let allSalary = result.reduce((sum, current) => sum + current);
+  let allMidleSalary = Math.trunc(allSalary / arr.length);
+  return allMidleSalary;
 };
 
 console.log(getMiddleSalary(emplyeeConstructArr));
@@ -53,15 +53,15 @@ console.log(getMiddleSalary(emplyeeConstructArr));
 // The sixth task
 
 const getRandomEmployee = (arr) => {
-    let factor = 10;
-    if (arr.length - 1 > 10) {
-        factor = 100;
-    }
-    let randomNumber = Math.round(Math.random() * factor);
-    if (randomNumber > arr.length - 1) {
-        getRandomEmployee(arr);
-    }
-    return arr[randomNumber];
+  let factor = 10;
+  if (arr.length - 1 > 10) {
+    factor = 100;
+  }
+  let randomNumber = Math.round(Math.random() * factor);
+  if (randomNumber > arr.length - 1) {
+    getRandomEmployee(arr);
+  }
+  return arr[randomNumber];
 };
 
 console.log(getRandomEmployee(emplyeeConstructArr));
@@ -70,17 +70,24 @@ console.log(getRandomEmployee(emplyeeConstructArr));
 
 const employeeObj = new Emploee(emplyeeArr[0]);
 
-Emploee.prototype.fullInfo = function() {
-    let result;
+Object.defineProperty(Emploee.prototype, "fullInfo", {
+  get: function () {
+    let result = "";
     for (let key in this) {
-        if (typeof(this[key]) === 'function') {
-            continue;
-        }
-        result += ` ${key} - ${this[key]}, `;
-
+      if (typeof this[key] !== "function") {
+        result += `${key} - ${this[key]}, `;
+      }
     }
     return result;
+  },
 
-};
-
-console.log(employeeObj.fullInfo());
+  set(value) {
+    for (let key in value) {
+      if (this.hasOwnProperty(key)) {
+        this[key] = value[key];
+      }
+    }
+  },
+});
+employeeObj.fullInfo = { salary: 4525 };
+console.log(employeeObj.fullInfo);
